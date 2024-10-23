a832a797ebc8554fcc210e75af51d2d15b370336
Stradalec <pochtatipo6@gmail.com>
2024-10-23 09:20:52 +0700

    Added final parts to Task2

--- app/src/main/java/com/example/android5lab/MainActivity.kt
+++ app/src/main/java/com/example/android5lab/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -24,7 +26,9 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
-
+        val searchButton: Button = findViewById(R.id.btn_search)
+        val searchEditText: EditText = findViewById(R.id.et_search)
+        lateinit var MyAdapter: ContactAdapter
         val client = OkHttpClient()
         val request = Request.Builder()
             .url("https://drive.google.com/u/0/uc?id=1-KO-9GA3NzSgIc1dkAsNm8Dqw0fuPxcR&=download").build()
@@ -35,13 +39,15 @@ class MainActivity : AppCompatActivity() {
                 if (body != null) {
                     val gson = GsonBuilder().create()
                     val wrapper: List<Contact> = gson.fromJson(body, Array<Contact>::class.java).toList()
-                    Timber.d("Thread")
+                    Timber.d("Русский язык")
                     val ContactList: List<Contact> = wrapper
                     wrapper.forEach { contact ->
                         Timber.d("Name: ${contact.name}, Phone: ${contact.phone}, Type: ${contact.type}")
                     }
                     runOnUiThread {
-                        displayAll(ContactList)
+                        MyAdapter = ContactAdapter(ContactList)
+                        MyAdapter.saveList(ContactList)
+                        displayAll(MyAdapter)
                     }
                 }
 
@@ -51,12 +57,17 @@ class MainActivity : AppCompatActivity() {
                 Timber.d("Request not work")
             }
         }.start()
+
+        searchButton.setOnClickListener{
+            val filter = searchEditText.text.toString()
+            Timber.d("Search started")
+            MyAdapter.filter(filter)
+        }
     }
-    private  fun displayAll( inputContacts: List<Contact> ) {
+    private  fun displayAll(inputAdapter: ContactAdapter) {
         val recyclerView: RecyclerView = findViewById(R.id.rView)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        var ContactAdapter =  ContactAdapter(inputContacts)
-        recyclerView.adapter = ContactAdapter
+        recyclerView.adapter = inputAdapter
     }
 }
 
@@ -66,7 +77,8 @@ data class Contact(
     val type: String
 )
 
-class ContactAdapter(private  val contacts: List<Contact>) : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>(){
+class ContactAdapter(private  var contacts: List<Contact>) : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>(){
+    private  var privateContactList: List<Contact> = contacts
     class  ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textName: TextView = itemView.findViewById(R.id.textName)
         val textPhone: TextView = itemView.findViewById(R.id.textPhone)
@@ -87,5 +99,28 @@ class ContactAdapter(private  val contacts: List<Contact>) : RecyclerView.Adapte
         holder.textName.text = contact.name
         holder.textPhone.text = contact.phone
         holder.textType.text = contact.type
+    }
+
+    fun saveList(inputContacts: List<Contact>){
+        privateContactList = inputContacts
+        contacts = inputContacts
+        notifyDataSetChanged()
+    }
+
+    fun filter(filter: String){
+        Timber.d("Trying filter")
+        if (filter.isEmpty()) {
+             contacts = privateContactList
+            Timber.d("Nothing to filter")
+        } else {
+            contacts = privateContactList.filter {
+               it.name.contains(filter, ignoreCase = true) ||
+               it.phone.contains(filter,ignoreCase = true) ||
+               it.type.contains(filter, ignoreCase = true)
+           }
+            Timber.d("Something to filter")
+        }
+        notifyDataSetChanged()
+
     }
 }
