2170fb99d54f1c76d21c0d1742a9ebe5ab12189a
Stradalec <pochtatipo6@gmail.com>
2024-10-31 21:10:23 +0700

    Added some classes

--- app/src/main/java/com/example/android5lab/MainActivity.kt
+++ app/src/main/java/com/example/android5lab/MainActivity.kt
@@ -71,56 +71,5 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-data class Contact(
-    val name: String,
-    val phone: String,
-    val type: String
-)
 
-class ContactAdapter(private  var contacts: List<Contact>) : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>(){
-    private  var privateContactList: List<Contact> = contacts
-    class  ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textName: TextView = itemView.findViewById(R.id.textName)
-        val textPhone: TextView = itemView.findViewById(R.id.textPhone)
-        val textType: TextView = itemView.findViewById(R.id.textType)
-    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.rview_item,parent,false)
-        return  ContactViewHolder(view)
-    }
-
-    override fun getItemCount(): Int {
-        return contacts.size
-    }
-
-    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
-        val contact = contacts[position]
-        holder.textName.text = contact.name
-        holder.textPhone.text = contact.phone
-        holder.textType.text = contact.type
-    }
-
-    fun saveList(inputContacts: List<Contact>){
-        privateContactList = inputContacts
-        contacts = inputContacts
-        notifyDataSetChanged()
-    }
-
-    fun filter(filter: String){
-        Timber.d("Trying filter")
-        if (filter.isEmpty()) {
-             contacts = privateContactList
-            Timber.d("Nothing to filter")
-        } else {
-            contacts = privateContactList.filter {
-               it.name.contains(filter, ignoreCase = true) ||
-               it.phone.contains(filter,ignoreCase = true) ||
-               it.type.contains(filter, ignoreCase = true)
-           }
-            Timber.d("Something to filter")
-        }
-        notifyDataSetChanged()
-
-    }
-}
